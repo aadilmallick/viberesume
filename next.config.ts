1421e81dd9e824b307ea18bfe95e54b67e82c5a2
aadilmallick <aadiltj.mallick@gmail.com>
2025-09-27 21:00:34 -0400

    add ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverComponentsExternalPackages: ["pdf-parse"],
+  ignoreBuildErrors: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
